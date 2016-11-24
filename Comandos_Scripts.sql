30f8461f8fbc60f9af87879301749af425d2c673
Lucas Flaquer <lucas.flaquer@gmail.com>
2016-11-24 10:47:33 -0200

    Consultas criadas

---
+++ Comandos_Scripts.sql
@@ -0,0 +1,40 @@
+
+
+Comandos SQL:
+4.1 Acrescente uma coluna nova “data de inclusão” no formato date, na primeira tabela
+criada. Altere o valor desta coluna colocando a data do sistema.
+
+"OBJETIVO: Este comando irá acrescentar a coluna data_inclusao na tabela carro"
+
+ALTER TABLE Carro ADD data_inclusao DATE;
+
+4.2 Escreva um comando Select utilizando a cláusula LIKE.
+
+"OBJETIVO: Este comando "
+
+4.3 Escreva um comando Select que utilize a cláusula where com 2 condições.
+4.4 Escreva um comando que utilize a funções de data dentro da cláusula where.
+4.5 Escreva um exemplo de junção entre 2 tabelas que retorne várias linhas.
+4.6 Escreva um exemplo de junção entre 3 tabelas.
+4.7 De um exemplo de comando DML que utilize a função count(* ) .
+4.8 De um exemplo de comando DML que utilize a função Sum().
+4.9 Explique para que serve a claúsula group by e dê 2 exemplos de sua utilização.
+4.10 Explique para que serve a claúsula having e dê 1 exemplo de sua utilização.
+4.11 Explique para que serve a claúsula order by e dê 1 exemplo de sua utilização.
+
+
+
+Subconsultas - Para cada exemplo que você criar descreva antes o objetivo do comando.
+Exemplo: “Objetivo: Este comando vai selecionar todos os clientes que não tem pedidos”
+4.12 Dê exemplo de um comando utilizando subconsultas que utilize o operador = ou
+<,>, <= etc.
+4.13 Dê exemplo de um comando utilizando subconsultas que utilize o operador in.
+4.14 Dê exemplo de um comando utilizando subconsultas que utilize o operador not in.
+4.15 Dê exemplo de um comando utilizando subconsultas que utilize o operador exists
+4.16 Dê exemplo de um comando utilizando subconsultas que utilize o operador not
+exists.
+4.17 Dê exemplo de uma subconsulta utilizada dentro de um comando Update.
+4.18 Dê exemplo de uma subconsulta utilizada dentro de um comando Delete.
+
+
+
